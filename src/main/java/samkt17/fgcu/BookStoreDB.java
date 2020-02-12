204a5136b0fdd306712f57769069a11fecf8e0b6
SamKT17 <skthomas0339@eagle.fgcu.edu>
2020-02-12 18:10:07 -0500

    Pushed the files to the database.

--- src/main/java/samkt17/fgcu/BookStoreDB.java
+++ src/main/java/samkt17/fgcu/BookStoreDB.java
@@ -1,10 +1,19 @@
 package samkt17.fgcu;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class BookStoreDB {
 
@@ -12,7 +21,7 @@ public class BookStoreDB {
     Connection conn = null;
     try {
       Class.forName("org.sqlite.JDBC");
-      String URL = "jdbc:sqlite:BookStore.db";
+      String URL = "jdbc:sqlite:./src/Data/BookStore";
       conn = DriverManager.getConnection(URL);
     } catch (ClassNotFoundException | SQLException e) {
       e.printStackTrace();
@@ -20,5 +29,55 @@ public class BookStoreDB {
     return conn;
   }
 
+  public void csvToDB() throws IOException, CsvValidationException {
 
+    Reader read = Files.newBufferedReader(Paths.get("src/Data/bookstore_report2.csv"));
+    CSVReader csvr = new CSVReader(read);
+
+    String[] fileInfo;
+    while ((fileInfo = csvr.readNext()) != null) {
+      String sql =
+          "INSERT INTO book(isbn, book_title, author_name, publisher_name) "
+              + "VALUES (?, ?, ?, ?)";
+
+      try {
+
+        PreparedStatement ps = connection().prepareStatement(sql);
+        ps.setString(1, fileInfo[0]);
+        ps.setString(2, fileInfo[1]);
+        ps.setString(3, fileInfo[2]);
+        ps.setString(4, fileInfo[3]);
+
+        ps.executeUpdate();
+
+        ps.close();
+        connection().close();
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void jsonToDB() throws FileNotFoundException {
+    Gson gson = new Gson();
+    JsonReader jread = new JsonReader(new FileReader("src/Data/authors.json"));
+    AuthorParser[] authors = gson.fromJson(jread, AuthorParser[].class);
+
+    String sql = "INSERT INTO author(author_name, author_email, author_url) VALUES (?, ?, ?)";
+
+    try {
+
+      PreparedStatement ps = connection().prepareStatement(sql);
+
+      for (var element : authors) {
+        ps.setString(1, element.getName());
+        ps.setString(2, element.getEmail());
+        ps.setString(3, element.getUrl());
+        ps.executeUpdate();
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/main/java/samkt17/fgcu/Main.java
+++ src/main/java/samkt17/fgcu/Main.java
@@ -1,42 +1,14 @@
 package samkt17.fgcu;
 
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 public class Main {
   public static void main(String[] args) throws IOException, CsvException {
 
-    Reader read = Files.newBufferedReader(Paths.get("src/Data/SEOExample.csv"));
-    CSVReader csvr = new CSVReader(read);
-
-    String[] fileInfo;
-    while((fileInfo = csvr.readNext()) != null) {
-      System.out.print(fileInfo[0] + " ");
-      System.out.print(fileInfo[1] + " ");
-      System.out.print(fileInfo[2] + " ");
-      System.out.print(fileInfo[3] + " ");
-      System.out.print(fileInfo[4] + " ");
-      System.out.print(fileInfo[5] + " ");
-      System.out.print(fileInfo[6] + " ");
-      System.out.print(fileInfo[7] + " ");
-      System.out.print(fileInfo[8] + " ");
-      System.out.print(fileInfo[9] + "\n");
-      System.out.println("====================================");
-    }
-
-    Gson gson = new Gson();
-    JsonReader jread = new JsonReader(new FileReader("src/Data/authors.json"));
-    AuthorParser[] authors = gson.fromJson(jread, AuthorParser[].class);
-
-    for (var element : authors) {
-      System.out.println(element.getName());
-    }
+    BookStoreDB bsdb = new BookStoreDB();
+    bsdb.csvToDB();
+    bsdb.jsonToDB();
   }
 }
